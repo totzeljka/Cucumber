4595ca115d0b1a8d6fc7bc85ba687f48b67184ea
Tot Željka <120595782+totzeljka@users.noreply.github.com>
2023-01-25 14:16:44 +0100

    Add files via upload

---
+++ CucumberCourse/morning-fresh/src/test/java/com/pluralsight/bdd/loyalty_cards/SuperSmoothieProgramTestSuite.java
@@ -0,0 +1,13 @@
+package com.pluralsight.bdd.loyalty_cards;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:features/loyalty_cards",
+        glue = "com.pluralsight.bdd"
+)
+public class SuperSmoothieProgramTestSuite {
+}
